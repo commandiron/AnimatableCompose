8f4d9befe374431866dc36426fc35f1906e230df
Emir Demirli <commandiron@gmail.com>
2022-11-02 09:26:13 +0300

    version increased.

--- app/src/main/java/com/commandiron/animatablecompose/MainActivity.kt
+++ app/src/main/java/com/commandiron/animatablecompose/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {}
                 Box(Modifier.fillMaxSize(), Alignment.Center) {
-                    Show4AnimatableCardWithText()
+                    Show5ExpandablePhoneNumber()
                 }
             }
         }
